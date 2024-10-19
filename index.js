e4dedec0f66106ff406fc7c83be80a82b0b9478c
Priyashan Gimantha <120010882+EroseEmrys@users.noreply.github.com>
2024-10-19 21:27:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (token) {
-    jwt.verify(token, "secret", (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         return res.status(401).json({ message: "Invalid Token" });
       }
